63be3e0fc84802355b89fbbcdb3c5a88f6477a4a
Sandhya Timalsena <sandhyatimalsena29@gmail.com>
2025-08-24 18:23:19 +0545

    started varieties page

---
+++ app/varieties/page.js
@@ -0,0 +1,43 @@
+import { ChevronDown } from 'lucide-react'
+import React from 'react'
+
+const Varieties = () => {
+    return (
+        <div className='relative top-21 bg-gradient-to-b from-green-50  to-white min-h-screen'>
+            <div className="max-w-3xl mx-auto text-center py-20 ">
+                <div className="inline-flex items-center bg-green-100 text-green-800 px-4 py-2 rounded-full text-sm font-medium mb-6">
+                    🍎 Premium Apple Collection
+                </div>
+                <h2 className="text-4xl md:text-5xl font-bold text-gray-900 mb-4">
+                    Our Apple Varieties
+                </h2>
+                <p className="text-xl text-gray-700">
+                    Hand-picked from our orchards every morning. Premium quality apples delivered fresh
+                    to your doorstep with guaranteed freshness and exceptional taste.
+                </p>
+            </div>
+            <div className="max-w-7xl mx-auto flex justify-between items-center mb-12 px-6">
+                <p className='text-gray-500'>Showing
+                    <span className='text-black font-semibold mx-1'>5</span>
+                    Varieties
+                </p>
+                <div className='relative'>
+                    <button
+                        className='flex items-center gap-3 bg-white border border-gray-300 rounded-xl px-4 py-3
+             text-gray-700 font-medium hover:border-gray-400 focus:outline-none focus:ring-2 
+             focus:ring-red-500 focus:border-transparent transition-all shadow-sm 
+             min-w-[200px] justify-between'
+                    >
+                        <span>Our Order</span>
+                        <ChevronDown />
+                    </button>
+
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Varieties
--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -3,55 +3,56 @@ import { Apple, Search, User, ShoppingBag } from "lucide-react";
 import Link from 'next/link';
 
 const Navbar = () => {
-  return (
-   <nav className="fixed w-full top-0 z-50 transition-all duration-500 bg-white/60 backdrop-blur-sm">
-    <div className='max-w-7xl mx-auto flex items-center justify-between px-6 py-4'>
-        <div className='flex items-center space-x-2'>
-            {/* Logo starts here */}
-            <div className='bg-red-600 p-2 space-x-2 rounded-2xl'>
-                <Apple className='text-white '/>
+    return (
+        <nav className="fixed w-full top-0 z-50 transition-all duration-500 bg-white/60 backdrop-blur-sm">
+            <div className='max-w-7xl mx-auto flex items-center justify-between px-6 py-4'>
+                <div className='flex items-center space-x-2'>
+                    {/* Logo starts here */}
+                    <div className='bg-red-600 p-2 space-x-2 rounded-2xl'>
+                        <Apple className='text-white ' />
+                    </div>
+
+                    <div className='flex flex-col leading-tight'>
+                        <span className='text-red-700 font-bold text-2xl'>Appletale</span>
+                        <span className='text-green-600 font-medium text-sm'>FARMS</span>
+                    </div>
+                </div>
+
+                {/* Navlinks starts here */}
+
+                <ul className='flex space-x-12 text-gray-600 font-medium'>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/varieties">Varieties</Link></li>
+                    <li><Link href="/about">About</Link></li>
+                    <li><Link href="/contact">Contact</Link></li>
+                </ul>
+
+
+                {/* Icons starts here */}
+                <div className='flex items-center space-x-6 text-gray-600'>
+                    <Search className='w-5 h-5' />
+                    <User className='w-5 h-5' />
+                    <ShoppingBag className='w-5 h-5' />
+
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+
             </div>
 
-            <div className='flex flex-col leading-tight'>
-                <span className='text-red-700 font-bold text-2xl'>Appletale</span>
-                <span className='text-green-600 font-medium text-sm'>FARMS</span>
-            </div>
-        </div>
-
-        {/* Navlinks starts here */}
-
-        <ul className='flex space-x-12 text-gray-600 font-medium'>
-            <li><Link href="/"></Link>Home</li>
-             <li><Link href="/"></Link>Varieties</li>
-              <li><Link href="/"></Link>About</li>
-               <li><Link href="/"></Link>Contact</li>
-        </ul>
-
-        {/* Icons starts here */}
-        <div className='flex items-center space-x-6 text-gray-600'>
-            <Search className='w-5 h-5'/>
-            <User className='w-5 h-5'/>
-            <ShoppingBag className='w-5 h-5'/>
-
-        </div>
 
 
-
-
-
-
-
-
-
-
-       
-
-    </div>
-
-
-
-   </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
