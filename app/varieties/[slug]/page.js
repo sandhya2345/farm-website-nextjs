acf73322fa1d3c24c8ac0a7c5ecd679eeb039825
Sandhya Timalsena <sandhyatimalsena29@gmail.com>
2025-09-04 07:54:53 +0545

    seperate vareity details tab

--- app/varieties/[slug]/page.js
+++ app/varieties/[slug]/page.js
@@ -1,12 +1,12 @@
 "use client"
 
-import { varieties } from '@/lib/data/varieties'
 import Recommendation from "@/components/ui/Recommendation/page";
-import { ArrowLeft, ArrowRight, Award, CheckCircle, CircleQuestionMark, Leaf, Minus, Plus, Shield, ShoppingCart, Truck } from 'lucide-react'
+import { ArrowLeft, Award, CircleQuestionMark, Leaf, Minus, Plus, Shield, ShoppingCart, Truck } from 'lucide-react'
 import Image from 'next/image'
 import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { notFound } from 'next/navigation';
+import VarietyTabs from "@/components/ui/Variety/page";
 
 const VarietyDetails = () => {
   const params = useParams();
@@ -14,10 +14,8 @@ const VarietyDetails = () => {
 
   const [variety, setVariety] = useState(null);
   const [loading, setLoading] = useState(true);
-  
-  const [clickedApple, setClickedApple] = useState("");
-  const [switchTap, setSwitchTap] = useState("description");
 
+  const [clickedApple, setClickedApple] = useState("");
   useEffect(() => {
     fetch(`/api/varieties/${slug}`)
       .then((res) => res.json())
@@ -30,10 +28,10 @@ const VarietyDetails = () => {
   }, [slug]);
 
   if (loading) return <p className="text-center py-20">Wait.....</p>;
-  if (!variety){
-        return notFound()
+  if (!variety) {
+    return notFound()
 
-    }
+  }
 
 
   return (
@@ -77,8 +75,6 @@ const VarietyDetails = () => {
                 <span className='bg-blue-600 text-white py-1 px-6 text-sm font-medium rounded-xl'>{variety.title.split(" ")[1]}</span>
               </div>
             </div>
-
-
 
 
             <div className='mt-4 flex gap-3 flex-wrap justify-center'>
@@ -201,162 +197,8 @@ const VarietyDetails = () => {
 
         </div>
 
-        {/* right content finished here */}
 
-        <div className='mt-12'>
-          <div className='border-b border-gray-200 '>
-            <div className='overflow-x-auto'>
-
-              <nav className='flex space-x-6 lg:space-x-8 min-w-max px-1'>
-                <button onClick={() => setSwitchTap('description')}
-                  className={`py-4 px-1 text-sm lg:text-base font-medium border-b-2 transition-colors whitespace-nowrap
-                 ${switchTap === "description"
-                      ? 'border-red-600 text-red-600'
-                      : 'border-transparent text-gray-500 hover:text-gray-700'
-                    }`}>
-                  Description
-                </button>
-                <button
-                  onClick={() => setSwitchTap('nutrition')}
-                  className={`py-4 px-1 text-sm lg:text-base font-medium border-b-2 transition-colors whitespace-nowrap ${switchTap === "nutrition"
-                    ? 'border-red-600 text-red-600'
-                    : 'border-transparent text-gray-500 hover:text-gray-700'
-                    }`}
-                >
-                  Nutrition
-                </button>
-                <button
-                  onClick={() => setSwitchTap('details')}
-                  className={`py-4 px-1 text-sm lg:text-base font-medium border-b-2 transition-colors whitespace-nowrap ${switchTap === "details"
-                    ? 'border-red-600 text-red-600'
-                    : 'border-transparent text-gray-500 hover:text-gray-700'
-                    }`}
-                >
-                  Details
-                </button>
-              </nav>
-            </div>
-          </div>
-        </div>
-
-        <div className='mt-6 space-y-6 lg:space-y-8'>
-          {switchTap === "description" && (
-            <div>
-              <h2 className='font-bold text-2xl mb-4 text-gray-800'>Product Details</h2>
-              <p className='text-gray-600 text-medium leading-relaxed space-y-4 mb-6'>{variety.longDescription}</p>
-
-              <div>
-                <h4 className='font-bold text-xl text-gray-800 mb-4'>Key Highlights</h4>
-                <ul className='space-y-2'>
-                  {
-                    variety.highlights.map((point, index) => (
-                      <li key={index} className='text-gray-600 flex items-start gap-4 space-y-2 text-medium'>
-                        <CheckCircle className='text-green-600 w-5 h-5' />
-                        <span>{point}</span>
-
-                      </li>
-                    ))}
-
-                </ul>
-              </div>
-            </div>
-          )}
-
-          {
-            switchTap === "nutrition" && (
-              <div>
-                <h1 className='text-2xl font-bold text-gray-800 mb-6'>Nutritional Information</h1>
-                <div className='bg-white border border-gray-200 rounded-xl overflow-hidden'>
-                  <div className='bg-gray-50 px-6 py-4 border-b border-gray-200'>
-                    <h4 className='font-semibold text-gray-800'>Nutrition Facts (per 100g)</h4>
-                  </div>
-
-                  <div className='divide-y divide-gray-100'>
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>Calories</dt>
-                      <dd className='text-gray-600' >{variety.calories}</dd>
-                    </div>
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>
-                        Carbohydates</dt>
-                      <dd className='text-gray-600' >{variety.carbohydates}</dd>
-                    </div>
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>Fiber</dt>
-                      <dd className='text-gray-600' >{variety.fiber}</dd>
-                    </div>
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>Sugar</dt>
-                      <dd className='text-gray-600' >{variety.sugar}</dd>
-                    </div>
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>Vitamin C</dt>
-                      <dd className='text-gray-600' >{variety.vitamin_c}</dd>
-                    </div>
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>Potassium</dt>
-                      <dd className='text-gray-600' >{variety.potassium}</dd>
-                    </div>
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>Protein</dt>
-                      <dd className='text-gray-600' >{variety.protein}</dd>
-                    </div>
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>Fat</dt>
-                      <dd className='text-gray-600' >{variety.fat}</dd>
-                    </div>
-                  </div>
-
-
-                </div>
-              </div>
-            )
-          }
-
-
-          {
-            switchTap === "details" && (
-              <div>
-                <h1 className='text-2xl font-bold text-gray-800 mb-6'>Product Specifications</h1>
-                <div className='bg-white border border-gray-200 rounded-xl overflow-hidden'>
-                  <div className='bg-gray-50 px-6 py-4 border-b border-gray-200'>
-                    <h4 className='font-semibold text-gray-800'>Product Information</h4>
-                  </div>
-                  <div className='divide-y divide-gray-100'>
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>Origin</dt>
-                      <dd className='text-gray-600' >{variety.calories}</dd>
-                    </div>
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>
-                        Variety</dt>
-                      <dd className='text-gray-600' >{variety.carbohydates}</dd>
-                    </div>
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>Season</dt>
-                      <dd className='text-gray-600' >{variety.calories}</dd>
-                    </div>
-
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>Shelf Life</dt>
-                      <dd className='text-gray-600' >{variety.calories}</dd>
-                    </div>
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>Storage</dt>
-                      <dd className='text-gray-600' >{variety.calories}</dd>
-                    </div>
-                    <div className='px-6 py-4 flex justify-between'>
-                      <dt className='text-gray-600'>Storage</dt>
-                      <dd className='text-gray-600' >{variety.calories}</dd>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-
-            )
-          }
-        </div>
+        <VarietyTabs/>
 
         <Recommendation />
 
---
+++ components/ui/Variety/page.js
@@ -0,0 +1,195 @@
+import { CheckCircle } from 'lucide-react';
+import { notFound, useParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
+
+const VarietyTabs = () => {
+    const [switchTab, setSwitchTab] = useState("description");
+    const { slug } = useParams();
+
+    const [variety, setVariety] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch(`/api/varieties/${slug}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setVariety(data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.error(err);
+                setLoading(false);
+            });
+    }, [slug]);
+
+    if (loading) {
+        return <p className="text-center py-20">Loading...</p>;
+    }
+
+    if (!variety) {
+        return <p className="text-center py-20">Variety not found.</p>;
+    }
+
+
+    return (
+        <div>
+            <div className='mt-12'>
+                <div className='border-b border-gray-200 '>
+                    <div className='overflow-x-auto'>
+
+                        <nav className='flex space-x-6 lg:space-x-8 min-w-max px-1'>
+                            <button onClick={() => setSwitchTab('description')}
+                                className={`py-4 px-1 text-sm lg:text-base font-medium border-b-2 transition-colors whitespace-nowrap
+                 ${switchTab === "description"
+                                        ? 'border-red-600 text-red-600'
+                                        : 'border-transparent text-gray-500 hover:text-gray-700'
+                                    }`}>
+                                Description
+                            </button>
+                            <button
+                                onClick={() => setSwitchTab('nutrition')}
+                                className={`py-4 px-1 text-sm lg:text-base font-medium border-b-2 transition-colors whitespace-nowrap ${switchTab === "nutrition"
+                                    ? 'border-red-600 text-red-600'
+                                    : 'border-transparent text-gray-500 hover:text-gray-700'
+                                    }`}
+                            >
+                                Nutrition
+                            </button>
+                            <button
+                                onClick={() => setSwitchTab('details')}
+                                className={`py-4 px-1 text-sm lg:text-base font-medium border-b-2 transition-colors whitespace-nowrap ${switchTab === "details"
+                                    ? 'border-red-600 text-red-600'
+                                    : 'border-transparent text-gray-500 hover:text-gray-700'
+                                    }`}
+                            >
+                                Details
+                            </button>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+
+            <div className='mt-6 space-y-6 lg:space-y-8'>
+                {switchTab === "description" && (
+                    <div>
+                        <h2 className='font-bold text-2xl mb-4 text-gray-800'>Product Details</h2>
+                        <p className='text-gray-600 text-medium leading-relaxed space-y-4 mb-6'>{variety.longDescription}</p>
+
+                        <div>
+                            <h4 className='font-bold text-xl text-gray-800 mb-4'>Key Highlights</h4>
+                            <ul className='space-y-2'>
+                                {
+                                    variety.highlights.map((point, index) => (
+                                        <li key={index} className='text-gray-600 flex items-start gap-4 space-y-2 text-medium'>
+                                            <CheckCircle className='text-green-600 w-5 h-5' />
+                                            <span>{point}</span>
+
+                                        </li>
+                                    ))}
+
+                            </ul>
+                        </div>
+                    </div>
+                )}
+
+                {
+                    switchTab === "nutrition" && (
+                        <div>
+                            <h1 className='text-2xl font-bold text-gray-800 mb-6'>Nutritional Information</h1>
+                            <div className='bg-white border border-gray-200 rounded-xl overflow-hidden'>
+                                <div className='bg-gray-50 px-6 py-4 border-b border-gray-200'>
+                                    <h4 className='font-semibold text-gray-800'>Nutrition Facts (per 100g)</h4>
+                                </div>
+
+                                <div className='divide-y divide-gray-100'>
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>Calories</dt>
+                                        <dd className='text-gray-600' >{variety.calories}</dd>
+                                    </div>
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>
+                                            Carbohydates</dt>
+                                        <dd className='text-gray-600' >{variety.carbohydates}</dd>
+                                    </div>
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>Fiber</dt>
+                                        <dd className='text-gray-600' >{variety.fiber}</dd>
+                                    </div>
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>Sugar</dt>
+                                        <dd className='text-gray-600' >{variety.sugar}</dd>
+                                    </div>
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>Vitamin C</dt>
+                                        <dd className='text-gray-600' >{variety.vitamin_c}</dd>
+                                    </div>
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>Potassium</dt>
+                                        <dd className='text-gray-600' >{variety.potassium}</dd>
+                                    </div>
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>Protein</dt>
+                                        <dd className='text-gray-600' >{variety.protein}</dd>
+                                    </div>
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>Fat</dt>
+                                        <dd className='text-gray-600' >{variety.fat}</dd>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    )
+                }
+
+
+                {
+                    switchTab === "details" && (
+                        <div>
+                            <h1 className='text-2xl font-bold text-gray-800 mb-6'>Product Specifications</h1>
+                            <div className='bg-white border border-gray-200 rounded-xl overflow-hidden'>
+                                <div className='bg-gray-50 px-6 py-4 border-b border-gray-200'>
+                                    <h4 className='font-semibold text-gray-800'>Product Information</h4>
+                                </div>
+                                <div className='divide-y divide-gray-100'>
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>Origin</dt>
+                                        <dd className='text-gray-600' >{variety.calories}</dd>
+                                    </div>
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>
+                                            Variety</dt>
+                                        <dd className='text-gray-600' >{variety.carbohydates}</dd>
+                                    </div>
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>Season</dt>
+                                        <dd className='text-gray-600' >{variety.calories}</dd>
+                                    </div>
+
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>Shelf Life</dt>
+                                        <dd className='text-gray-600' >{variety.calories}</dd>
+                                    </div>
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>Storage</dt>
+                                        <dd className='text-gray-600' >{variety.calories}</dd>
+                                    </div>
+                                    <div className='px-6 py-4 flex justify-between'>
+                                        <dt className='text-gray-600'>Storage</dt>
+                                        <dd className='text-gray-600' >{variety.calories}</dd>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    )
+                }
+            </div>
+
+        </div>
+    )
+}
+
+export default VarietyTabs
