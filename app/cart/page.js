f5ca8ae4d28814791538785c6fdf9492d3fc6f81
Sandhya Timalsena <sandhyatimalsena29@gmail.com>
2025-09-04 15:04:38 +0545

    little progress on cart page

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,9 +1,29 @@
+import { ArrowLeft, Key, Lock } from 'lucide-react'
 import React from 'react'
 
 const Cart = () => {
   return (
-    <div className='py-20 flex justify-center'>
-        when will this phase of mine ends??????????????????
+    <div className='min-h-screen py-20  bg-white'>
+      <div className='max-w-7xl mx-auto px-4 py-4'>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center gap-3'>
+            <button>
+              <ArrowLeft/>
+            </button>
+            <div>
+              <h1 className='font-semibold text-gray-900 text-lg'>Shopping Cart</h1>
+              <p className='text-sm text-gray-600'>1 items</p>
+            </div>
+          </div>
+
+          <div className='flex items-center gap-2' >
+            <Lock className='w-4 h-4 text-green-600'/>
+            <span className='text-green-600 font-medium'>Secure</span>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   )
 }
