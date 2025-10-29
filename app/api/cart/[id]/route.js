a4b2674ddc8e2b0b658fc8b12577a80382278e05
Sandhya Timalsena <sandhyatimalsena29@gmail.com>
2025-10-29 11:22:27 +0545

    updated cart route.js filr

--- app/api/cart/[id]/route.js
+++ app/api/cart/[id]/route.js
@@ -1,3 +1,61 @@
+// import clientPromise from "@/lib/mongodb";
+// import { NextResponse } from "next/server";
+// import { ObjectId } from "mongodb";
+
+// export async function DELETE(req, { params }) {
+//   const client = await clientPromise;
+//   const db = client.db("appletales");
+
+//   const { id } = params;
+
+//   if (!ObjectId.isValid(id)) {
+//     return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+//   }
+
+//   const result = await db.collection("cart").deleteOne({ _id: new ObjectId(id) });
+
+//   if (result.deletedCount === 0) {
+//     return NextResponse.json({ error: "Item not found" }, { status: 404 });
+//   }
+
+//   return NextResponse.json({ success: true });
+// }
+
+
+// export async function PATCH(req, { params }) {
+//   try {
+//     const client = await clientPromise;
+//     const db = client.db("appletales");
+
+//     const { id } = params;
+//     const body = await req.json();
+//     const { quantity } = body;
+
+//     if (!ObjectId.isValid(id)) {
+//       return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+//     }
+
+//     if (!quantity || quantity < 1) {
+//       return NextResponse.json({ error: "Quantity must be atleast 1" }, { status: 400 });
+//     }
+
+//     const result = await db.collection("cart").updateOne(
+//       { _id: new ObjectId(id) },
+//       { $set: { quantity } }
+//     );
+
+//     if (result.itemCount === 0) {
+//       return NextResponse.json({ error: "Item not found" }, { status: 404 });
+//     }
+
+//     return NextResponse.json({ success: true });
+//   } catch (err) {
+//     console.error(err);
+//     return NextResponse.json({ error: "Failed to update cart" }, { status: 500 });
+//   }
+// }
+
+
 import clientPromise from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
@@ -21,7 +79,6 @@ export async function DELETE(req, { params }) {
   return NextResponse.json({ success: true });
 }
 
-
 export async function PATCH(req, { params }) {
   try {
     const client = await clientPromise;
@@ -44,7 +101,7 @@ export async function PATCH(req, { params }) {
       { $set: { quantity } }
     );
 
-    if (result.itemCount === 0) {
+    if (result.matchedCount === 0) {
       return NextResponse.json({ error: "Item not found" }, { status: 404 });
     }
 
